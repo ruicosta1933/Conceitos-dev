8d07170fac8790f8fb455b35e9b1530f7a4a2cfb
Xavi002 <diogomartins200214@gmail.com>
2020-07-06 22:08:18 +0100

    estado e imutabilidade

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  //useState retorna um array com 2 posições
+  //1. Variável com o seu valor inicial
+  //2. Função para atualizar esse valor
+
+  function handleAddProject() {
+
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]); //Copio o array projects e adiciono o novo
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
+      <Header title="Projects" />
+
       <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+        {projects.map(project => <li key={project}>{project}</li>)}  {/* Dada project dentro de projects, vai retornar um li com cada projeto */}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default function Header({ title, children }) { //Children é todo o conteúdo gerado dentro do componente
+export default function Header({ title }) { //Children é todo o conteúdo gerado dentro do componente
   return (
     <header>
       <h1>{title}</h1>
-
-      {children}
     </header>
   );
 }
