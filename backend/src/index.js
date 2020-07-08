698d511888bbbb92c988cc6b5661b8ba4a957536
Xavi002 <diogomartins200214@gmail.com>
2020-07-08 21:39:46 +0100

    Revert "react native base"
    This reverts commit 08fe0a9e750aede96ca29c171b6c5de36eba0264.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 const projects = [];  //Funciona como "DB" de testes
